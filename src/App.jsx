e112a8f46ec96adf5aaa36559bafe6d66bb14076
Aphelion-im <aphelion-im@protonmail.com>
2023-05-01 21:04:02 +0200

    Ádded week weather forecasts, Chapter React H7.7

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 // H6.4: https://edhub.novi.nl/study/courses/516/content/12755 (Data fetchen)
 // Deze App maakt gebruik van de volgende pagina's:
 // Components: SearchBar, TabBarMenu, WeatherDetail, MetricSlider
-// Pages: ForecastTab, TodayTab
+// Pages: App.jsx, ForecastTab, TodayTab
+// @H7.7
 
 import './App.css';
 import axios from 'axios';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
-import { useState } from 'react';
+import ForecastTab from './pages/forecastTab/ForecastTab';
+import { useState, useEffect } from 'react';
 
 // Variables
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
@@ -16,29 +18,35 @@ const PUBLIC_KEY = import.meta.env.VITE_APP_PUBLIC_KEY;
 
 export default function App() {
   const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState('');
 
-  async function fetchData() {
-    try {
-      const result = await axios.get(
-        `${BASE_URL}?q=utrecht,nl&appid=${PUBLIC_KEY}&lang=nl`
-      );
-      console.log(result.data);
-      setWeatherData(result.data);
-    } catch (e) {
-      console.error(e);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}weather?q=${location},nl&appid=${PUBLIC_KEY}&lang=nl`
+        );
+        console.log(response.data);
+        setWeatherData(response.data);
+      } catch (e) {
+        console.error(e);
+      }
     }
-  }
+    // React H7.5: We roepen de functie aan als location is veranderd, maar niet als het een null/undefined/lege string is
+    // Alleen fetchForecasts aanroepen als er daadwerkelijk coordinaten beschikbaar zijn, ivm asynchroon
+    if (location) {
+      fetchData();
+    }
+  }, [location]);
 
   return (
     <>
       <div className="weather-container">
         {/*HEADER -------------------- */}
         <div className="weather-header">
-          <SearchBar />
+          <SearchBar setLocationHandler={setLocation} />
 
           <span className="location-details">
-            {/* Object.keys:  */}
-            {/* Maar omdat wij weatherData initialiseren als leeg object (const [weatherData, setWeatherData] = useState({});), zal dit in bovenstaande check evalueren naar truthy en krijgen we alsnog een foutmelding. Wanneer we structurele datatypes gebruiken (zoals objecten en arrays) zullen we moeten checken of er wel keys of waardes in staan, voor we elementen renderen */}
             {Object.keys(weatherData).length > 0 && (
               <>
                 <h2>{weatherData.weather[0].description}</h2>
@@ -46,10 +54,6 @@ export default function App() {
                 <h1>{weatherData.main.temp}</h1>
               </>
             )}
-
-            <button onClick={fetchData} type="button">
-              Haal data op!
-            </button>
           </span>
         </div>
 
@@ -58,7 +62,7 @@ export default function App() {
           <TabBarMenu />
 
           <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
+            <ForecastTab coordinates={weatherData.coord} />
           </div>
         </div>
 
--- src/pages/forecastTab/ForecastTab.jsx
+++ src/pages/forecastTab/ForecastTab.jsx
@@ -1,83 +1,60 @@
-import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './ForecastTab.css';
 
-function ForecastTab() {
+// Variables
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+const PUBLIC_KEY = import.meta.env.VITE_APP_PUBLIC_KEY;
+
+function createDateString(timestamp) {
+  const day = new Date(timestamp * 1000);
+  return day.toLocaleDateString('nl-NL', { weekday: 'long' });
+}
+
+function ForecastTab({ coordinates }) {
+  const [forecasts, setForecasts] = useState([]);
+
+  useEffect(() => {
+    async function fetchForecasts() {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${PUBLIC_KEY}&lang=nl`
+        );
+        console.log(response.data);
+
+        const fiveDayForecast = response.data.list.filter((singleForecast) => {
+          return singleForecast.dt_txt.includes('12:00:00');
+        });
+
+        setForecasts(fiveDayForecast);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    if (coordinates) {
+      fetchForecasts();
+    }
+  }, [coordinates]);
+
   return (
     <div className="tab-wrapper">
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
+      {forecasts.map((singleForecast) => {
+        return (
+          <article className="forecast-day" key={singleForecast.dt}>
+            <p className="day-description">
+              {createDateString(singleForecast.dt)}
+            </p>
 
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
-
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
-
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
-
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
-
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
-
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
-
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
-
-      <article className="forecast-day">
-        <p className="day-description">
-          Maandag
-        </p>
-
-        <section className="forecast-weather">
-            <span>
-              12&deg; C
-            </span>
-          <span className="weather-description">
-              Licht Bewolkt
-            </span>
-        </section>
-      </article>
+            <section className="forecast-weather">
+              <span>{singleForecast.main.temp}&deg; C</span>
+              <span className="weather-description">
+                {singleForecast.weather[0].description}
+              </span>
+            </section>
+          </article>
+        );
+      })}
     </div>
   );
 }
